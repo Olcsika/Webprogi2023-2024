3609710daa8812cc402abb1820b6f816e2112634
Lébár Olivér <lebaro21@ady-nagyatad.sulinet.hu>
2025-02-18 09:30:21 +0100

    a

--- fetch api doga jqueryvel/script.js
+++ fetch api doga jqueryvel/script.js
@@ -1,57 +1,60 @@
-//alert("CSő")
-
-$.ajax({url:"https://fakestoreapi.com/products", 
-    dataType: 'json',
-    success:function(data){
-        console.log(data)
+$.ajax({
+    url: "https://fakestoreapi.com/products",
+    dataType: "json",
+    success: function (data) {
+        console.log(data);
         data.forEach(product => {
-            console.log(product)
-            let kartya = termekKarty(product.title,product.image,product.description)
-            $("#termekLista").append(kartya)
+            console.log(product);
+            let kartya = termekKartya(product.id,product.title, product.image, product.description);
+            $("#termekLista").append(kartya);
         });
     }
 });
-function termekKarty(nev,kepurl,leiras)
-{
-    let keret = jQuery("<div>") 
-    keret.prop("class","card")
-    keret.addClass("col-3");   
 
-    let kep = jQuery("<img>")
-    kep.prop("src",kepurl)
-    kep.prop("class","card-img-top")
-    kep.prop("alt",nev)
-
-    let cardText=jQuery("<p>")
-    cardText.prop("class","card-text")
-    cardText.text(leiras)
+function termekKartya(id,nev,kepUrl,leiras) {
+    let kartya = jQuery("<div>");
+    kartya.prop("class","card");
+    kartya.addClass("col-3");
+    kartya.on("click", function(){togglePopup()});
     
-    let cardBody=jQuery("<div>")
-    cardBody.prop("class","card-body")
+    $.ajax({
+        url: "https://fakestoreapi.com/products/id",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            data.forEach(product => {
+                console.log(product);
+                let kartya = termekKartya(product.title, product.image, product.description);
+                $("#termekLista").append(kartya);
+            });
+        }
+    });
 
-    let cardTitle=jQuery("<h5>")
-    cardTitle.prop("class","card-title")
-    cardTitle.text(nev)
+    let kep = jQuery("<img>");
+    kep.prop("src", kepUrl);
+    kep.prop("class", "card-img-top");
+    kep.prop("alt", nev);
 
-    cardBody.append(cardText)
-    cardBody.append(cardTitle)
-    keret.append(kep)
-    keret.append(cardBody)
+    let cardBody = jQuery("<div>");
+    cardBody.prop("class", "card-body");
 
-    return keret;
-    
+    let cardTitle = jQuery("<h5>");
+    cardTitle.prop("card-title");
+    cardTitle.text(nev);
 
-   
-    
+    let cardText = jQuery("<p>");
+    cardText.prop("class", "card-text");
+    cardText.text(leiras);
 
+    cardBody.append(cardTitle);
+    cardBody.append(cardText);  
 
-/*
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  /div>
-</div>*/
+    kartya.append(kep);
+    kartya.append(cardBody);
+
+    return kartya;
+
+}
+function togglePopup() {
+    $(".content").toggle();
 }
